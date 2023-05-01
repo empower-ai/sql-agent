8b35da376ef1897f8b1560472154f4cc275ae2ca
Yulong Liu <lyl890808@gmail.com>
2023-05-01 10:51:53 -0700

    bug

--- src/index.ts
+++ src/index.ts
@@ -14,9 +14,6 @@ import DataQuestionAgent from './agent/data-question-agent.js';
 import loadDataSource from './datasource/index.js';
 import { buildDataSourceContextIndex } from './indexes/index.js';
 import { createSSHTunnelIfNecessary } from './utils/ssh-tunnel.js';
-import handleEditAssumptions from './slack/event-handlers/edit-assumptions.js';
-import handleCancelAssumptionsEditing from './slack/event-handlers/cancel-assumptions-editing.js';
-import handleUpdateAssumptions from './slack/event-handlers/update-assumptions.js';
 
 const NODE_MAJOR_VERSION = parseInt(process.versions.node.split('.')[0]);
 if (NODE_MAJOR_VERSION < 18) {
@@ -44,25 +41,36 @@ const dataSource = loadDataSource();
 const dataSourceContextIndex = buildDataSourceContextIndex(Boolean(process.env.ENABLE_EMBEDDING_INDEX), dataSource);
 const dataQuestionAgent = new DataQuestionAgent(dataSource, dataSourceContextIndex);
 
-const app = new App({
-  signingSecret: process.env.SLACK_SIGNING_SECRET,
-  token: process.env.SLACK_BOT_TOKEN,
-  appToken: process.env.SLACK_APP_TOKEN,
-  socketMode: true
-});
+logger.info('initialization complete');
+async function slackBot(): Promise<void> {
+  const app = new App({
+    signingSecret: process.env.SLACK_SIGNING_SECRET,
+    token: process.env.SLACK_BOT_TOKEN,
+    appToken: process.env.SLACK_APP_TOKEN,
+    socketMode: true
+  });
 
+  await Promise.all([
+    handleAppMention(app, dataQuestionAgent),
+    handleEditQuery(app),
+    handleCancelQueryEditing(app),
+    handleRunEditedQuery(app, dataSource),
+    handleCommand(app, dataSource)
+  ]);
+}
+
+async function webApp(): Promise<void> {
+  logger.info('Preparing Next.js app')
+
+  const next = require('next')
+
+  const app = next({});
+  await app.prepare();
+  logger.info('Next.js app prepared');
+}
+
+logger.info('Starting Slack app and Next.js app');
 await Promise.all([
-  handleAppMention(app, dataQuestionAgent),
-  handleEditQuery(app),
-  handleCancelQueryEditing(app),
-  handleRunEditedQuery(app, dataSource),
-  handleCommand(app, dataSource),
-  handleEditAssumptions(app),
-  handleCancelAssumptionsEditing(app),
-  handleUpdateAssumptions(app, dataQuestionAgent)
-]);
-
-const port = process.env.PORT ?? 3000;
-await app.start();
-
-logger.info(`Sensei is up running, listening on port ${port}`);
+  slackBot(),
+  webApp()
+])
---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  )
+}
+
+export default Home
