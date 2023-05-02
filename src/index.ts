1fac27e8f023c42e9f4b4e900260ca8369ca905c
Yulong Liu <lyl890808@gmail.com>
2023-05-01 23:07:59 -0700

    add a feature flag for web app

--- src/index.ts
+++ src/index.ts
@@ -47,47 +47,45 @@ const dataSourceContextIndex = buildDataSourceContextIndex(Boolean(process.env.E
 const questionAssumptionIndex = new QuestionAssumptionIndex();
 const dataQuestionAgent = new DataQuestionAgent(dataSource, dataSourceContextIndex, questionAssumptionIndex);
 
-const slackApp = new App({
-  signingSecret: process.env.SLACK_SIGNING_SECRET,
-  token: process.env.SLACK_BOT_TOKEN,
-  appToken: process.env.SLACK_APP_TOKEN,
-  socketMode: true
-});
-await Promise.all([
-  handleAppMention(slackApp, dataQuestionAgent),
-  handleEditQuery(slackApp),
-  handleCancelQueryEditing(slackApp),
-  handleRunEditedQuery(slackApp, dataSource),
-  handleCommand(slackApp, dataSource)
-]);
+async function startSlackApp(): Promise<void> {
+  const slackApp = new App({
+    signingSecret: process.env.SLACK_SIGNING_SECRET,
+    token: process.env.SLACK_BOT_TOKEN,
+    appToken: process.env.SLACK_APP_TOKEN,
+    socketMode: true
+  });
+  await Promise.all([
+    handleAppMention(slackApp, dataQuestionAgent),
+    handleEditQuery(slackApp),
+    handleCancelQueryEditing(slackApp),
+    handleRunEditedQuery(slackApp, dataSource),
+    handleCommand(slackApp, dataSource)
+  ]);
 
-slackApp.start().then(() => {
+  await slackApp.start();
   logger.info('⚡️ Bolt app started');
-}).catch((error) => {
-  logger.error(error);
-  process.exit(1);
-});
+}
 
-logger.info('Preparing Next.js app')
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const dev = process.env.NODE_ENV !== 'production'
-const hostname = 'localhost'
-const port = 3000
+async function startWebApp(): Promise<void> {
+  logger.info('Preparing Next.js app')
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const dev = process.env.NODE_ENV !== 'production'
+  const hostname = 'localhost'
+  const port = 3000
 
-logger.info(`dev: ${dev}, hostname: ${hostname}, port: ${port}`)
+  const webApp = next({ dev, hostname, port });
+  const handle = webApp.getRequestHandler()
 
-const webApp = next({ dev, hostname, port });
-const handle = webApp.getRequestHandler()
-
-webApp.prepare().then(() => {
+  await webApp.prepare();
   createServer((req, res) => {
     const parsedUrl = parse(req.url!, true)
     handle(req, res, parsedUrl)
   }).listen(port)
 
-  console.log(
-    `> Server listening at http://localhost:${port} as ${
-      dev ? 'development' : process.env.NODE_ENV
-    }`
-  )
-})
+  logger.info(`⚡️ Next.js app started on http://${hostname}:${port}`);
+}
+
+await startSlackApp();
+if (process.env.ENABLE_WEB_APP === 'true') {
+  await startWebApp();
+}
