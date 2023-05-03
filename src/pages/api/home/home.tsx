161090a7d95b1743819508ade8c8eecc9d9b48b2
Yulong Liu <lyl890808@gmail.com>
2023-05-02 21:22:33 -0700

    uncomment

--- src/pages/api/home/home.tsx
+++ src/pages/api/home/home.tsx
@@ -369,22 +369,22 @@ const Home = ({
         <main
           className={`flex h-screen w-screen flex-col text-sm text-white dark:text-white ${lightMode}`}
         >
-          {/* <div className="fixed top-0 w-full sm:hidden">
+          <div className="fixed top-0 w-full sm:hidden">
             <Navbar
               selectedConversation={selectedConversation}
               onNewConversation={handleNewConversation}
             />
-          </div> */}
+          </div>
 
-          {/* <div className="flex h-full w-full pt-[48px] sm:pt-0"> */}
+          <div className="flex h-full w-full pt-[48px] sm:pt-0">
             <Chatbar />
 
-            {/* <div className="flex flex-1">
+            <div className="flex flex-1">
               <Chat stopConversationRef={stopConversationRef} />
             </div>
 
-            <Promptbar /> */}
-          {/* </div> */}
+            <Promptbar />
+          </div>
         </main>
       )}
     </HomeContext.Provider>
