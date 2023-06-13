2feb8cfa79dda292e6c4a5a69025a0716fa4d358
Yulong Liu <lyl890808@gmail.com>
2023-06-13 12:25:31 -0700

    remove some unneeded components

--- src/web/components/Chat/Chat.tsx
+++ src/web/components/Chat/Chat.tsx
@@ -288,45 +288,8 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
 
   return (
     <div className="relative flex-1 overflow-hidden bg-white dark:bg-[#343541]">
-      {!(apiKey || serverSideApiKeyIsSet)
-        ? (
-          <div className="mx-auto flex h-full w-[300px] flex-col justify-center space-y-6 sm:w-[600px]">
-            <div className="text-center text-4xl font-bold text-black dark:text-white">
-              Welcome to Chatbot UI
-            </div>
-            <div className="text-center text-lg text-black dark:text-white">
-              <div className="mb-8">{'Chatbot UI is an open source clone of OpenAI\'s ChatGPT UI.'}</div>
-              <div className="mb-2 font-bold">
-                Important: Chatbot UI is 100% unaffiliated with OpenAI.
-              </div>
-            </div>
-            <div className="text-center text-gray-500 dark:text-gray-400">
-              <div className="mb-2">
-                Chatbot UI allows you to plug in your API key to use this UI with
-                their API.
-              </div>
-              <div className="mb-2">
-                It is <span className="italic">only</span> used to communicate
-                with their API.
-              </div>
-              <div className="mb-2">
-                Please set your OpenAI API key in the bottom left of the sidebar.
-              </div>
-              <div>
-                If you don't have an OpenAI API key, you can get one here:
-                <a
-                  href="https://platform.openai.com/account/api-keys"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="text-blue-500 hover:underline"
-                >
-                  openai.com
-                </a>
-              </div>
-            </div>
-          </div>
-          )
-        : modelError
+      {
+        modelError
           ? (
             <ErrorMessageDiv error={modelError} />
             )
